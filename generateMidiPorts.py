5eeb2347a5b22918c28b123aea0d40a74a939d78
walt <walt>
2017-05-13 12:37:14 +0200

    support for multiple midi inputs with same name

--- generateMidiPorts.py
+++ generateMidiPorts.py
@@ -1,30 +1,3 @@
-import os
-import subprocess
-
-
-def findDevicePorts(hintName, fullString):
-
-  inputsStart = fullString.find("MIDI inputs")
-  outputsStart = fullString.find("MIDI outputs")
-
-  def findPort(searchStr):
-    ind = searchStr.lower().find(hintName.lower())
-    if ind == -1:
-      return -1
-    i1 = searchStr.rfind("[", 0, ind)
-    i2 = searchStr.find("]", i1)
-    return searchStr[i1+1:i2]
-  return findPort(fullString[inputsStart:outputsStart]), findPort(fullString[outputsStart:])
-
-
-
-
-
-proc = subprocess.Popen(["chuck", "--probe"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-chuckOutput = " ".join(proc.stderr.readlines())
-proc.wait()
-#print chuckOutput
-
 deviceHints = [
   ["through"]
   ,["launchpad"]
@@ -57,24 +30,69 @@ deviceHints = [
   ,["APC MINI", "apc"]
 ]
 
+import os
+import subprocess
+
+
+def findDevicePorts(hintName, fullString):
+  inputsStart = fullString.find("MIDI inputs")
+  outputsStart = fullString.find("MIDI outputs")
+
+  def findPort(searchStr):
+    ind = searchStr.lower().find(hintName.lower())
+    if ind == -1:
+      return (-1,-1)
+    i1 = searchStr.rfind("[", 0, ind)
+    i2 = searchStr.find("]", i1)
+    return (ind+1, searchStr[i1+1:i2])
+
+  curInStart = inputsStart
+  curOutStart = outputsStart
+
+  while True:
+    (inAdd, inPort) = findPort(fullString[curInStart:outputsStart])
+    curInStart += inAdd
+    (outAdd, outPort) = findPort(fullString[curOutStart:])
+    curOutStart += outAdd
+    if inPort == -1 and outPort == -1:
+      break
+    yield (inPort, outPort)
+
+
+
+def write(name, inOut, p):
+  outFile.write("define(MIDI_%s_%s, %s)\n" % 
+      (inOut.upper(), name.upper().replace(" ","_").replace("-", "_"), p))
+
+proc = subprocess.Popen(["chuck", "--probe"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+chuckOutput = " ".join(proc.stderr.readlines())
+proc.wait()
 
 import sys
 
 with open(sys.argv[1], "w") as outFile:
   for d in deviceHints:
-    portIn, portOut = findDevicePorts(d[0], chuckOutput)
-    name = d[0]
+    rootName = d[0]
     if len(d) > 1:
-      name = d[1]
-    def w(inOut,p):
-      outFile.write("define(MIDI_%s_%s, %s)\n" % 
-          (inOut.upper(), name.upper().replace(" ","_").replace("-", "_"), p))
-    if portIn != -1:
-      w("in", portIn)
-    else:
-      w("in", 32)
-    if portOut != -1:
-      w("out", portOut)
-    else:
-      w("out", 32)
+      rootName = d[1]
 
+    ports = [x for x in findDevicePorts(d[0], chuckOutput)]
+    if len(ports) == 0:
+        write(rootName, "in", 32)
+        write(rootName, "out", 32)
+    else:
+      for (ind, (portIn, portOut)) in enumerate(ports):
+        name = rootName+(ind > 0 and str(ind) or "")
+        write(name, "in", portIn==-1 and 32 or portIn)
+        write(name, "out", portOut==-1 and 32 or portOut)
+
+
+    # if portIn != -1:
+    #   w("in", portIn)
+    # else:
+    #   w("in", 32)
+    # if portOut != -1:
+    #   w("out", portOut)
+    # else:
+    #   w("out", 32)
+    #
